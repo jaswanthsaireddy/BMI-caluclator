7f9053df6b24285be20e3e7e945edaa5f1df1e9c
jaswanthsaireddy <pasalajaswanthsair8@gmail.com>
2022-06-23 16:19:06 +0530

    done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let caluclateButton = document.querySelector("#caluclate")
 
 // report data
 
-let catoegryData = document.querySelector("#catoegry-report-data")
+
 
 let nameData = document.querySelector("#name-report-data")
 
@@ -26,6 +26,8 @@ let editData = document.querySelector("#edit-report-data")
 
 let deleteData = document.querySelector("#delete-report-data")
 
+let report = document.querySelector(".report-row")
+
 
 // lets add an event listener to it
 
@@ -33,31 +35,66 @@ caluclateButton.addEventListener("click",caluclateBMIOfUser);
 
 
  function caluclateBMIOfUser(){
-    let sqaureOfHeight = parseFloat(userheight.value) * parseFloat(userheight.value)
-    let BMI = parseFloat(userWeight.value) / parseFloat(sqaureOfHeight)
+   let sqaureOfHeight = parseFloat(userheight.value) * parseFloat(userheight.value)
+
+   let BMI = parseFloat(userWeight.value) / parseFloat(sqaureOfHeight)
+
+   let catoegryData ;
     errorMessage.style.display = "none";
     if (parseFloat(BMI) > 0) {
        BMIResult.value = `${parseFloat(BMI)}`
-       nameData.innerHTML = `${userName.value}`
-       weightData.innerHTML = `${userWeight.value}`
-       heightData.innerHTML = `${userheight.value}`
-       BMIData.innerHTML = `${parseFloat(BMI)}`
+      //  nameData.innerHTML = `${userName.value}`
+      //  weightData.innerHTML = `${userWeight.value}`
+      //  heightData.innerHTML = `${userheight.value}`
+      //  BMIData.innerHTML = `${parseFloat(BMI)}`
        if (0< parseFloat(BMI)  && parseFloat(BMI)< 18.5) {
-        catoegryData.innerHTML = "Under-weight"    
+        catoegryData = "Under-weight";   
        }
        else if (18.5 <parseFloat(BMI) && parseFloat(BMI) < 24.9){
-        catoegryData.innerHTML = "Healthy-weight"
+        catoegryData = "Healthy-weight";
+        console.log
        }
        else if (25 <parseFloat(BMI) && parseFloat(BMI)< 29.9){
-        catoegryData.innerHTML = "Over-weight"
+        catoegryData = "Over-weight";
        }
        else if (parseFloat(BMI) > 30){
-        catoegryData.innerHTML = "Healthy-weight"
+        catoegryData = "Obese";
        }
     } else {
         errorMessage.innerHTML = "Please enter positive weights and heights"
         errorMessage.style.display = "block";
     }
     
+    displayReportCard(BMI,catoegryData);
 
+ }
+
+
+ function displayReportCard(BMI_value,catoegryData){
+   report.innerHTML += `<tr><td id="name-report-data">${userName.value}</td>
+   <td id="weight-report-data">${userWeight.value}</td>
+   <td id="height-report-data">${userheight.value}</td>
+   <td id="BMI-report-data">${parseFloat(BMI_value)}</td>
+   <td id="catoegry-report-data">${catoegryData}</td>
+   <td onclick = "editDataFromReport(this)" id="edit-report-data"><button id="edit">Edit</button></td>
+   <td onclick = "deleteDataFromReport(this)" id="delete-report-data"><button id="delete">Delete</button></td></tr>`
+   userName.value = ""
+   userheight.value = ""
+   userWeight.value = ""
+   BMIResult.value = ""
+
+
+
+ }
+
+ function deleteDataFromReport(k){
+   k.parentElement.remove()
+ }
+
+ function editDataFromReport(k){
+   k.parentElement.remove()
+   userName.value = k.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
+   userWeight.value = k.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
+   userheight.value = k.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
+   BMIResult.value = k.previousElementSibling.previousElementSibling.innerHTML;
  }
